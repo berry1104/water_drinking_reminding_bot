8cf02ecd658f3290ea66791cd906c9e931fb5bb9
berry1104 <935311076@qq.com>
2023-10-09 15:45:21 +0800

    done with convert distance to amount

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import math
 
 GPIO.setmode(GPIO.BOARD)
 PIN_TRIGGER = 7
@@ -9,6 +10,8 @@ button_pin = 19
 GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 GPIO.setup(PIN_ECHO, GPIO.IN)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+diameter = 10.0  # Diameter of the cup in centimeters
 
 GPIO.output(PIN_TRIGGER, GPIO.LOW)
 print("Waiting for sensor to settle")
@@ -30,23 +33,66 @@ def caculate_distance():
 
     distance = round(pulse_duration * 17150, 2)
 
-    print(f"distance is {distance} cm ")
+    
     return distance
 
-# print("First caculate the height of cup :  ")
-# height_cup = caculate_distance()
+
+
+def get_amount(d_before, d_after, d):
+    '''
+    d_before: distance before drinking water in cm
+    d_after: distance after drinking water in cm
+    d: diameter of the cup in cm
+    amount : amount of water in ml
+    '''
+    # Calculate the radius of the cup
+    r = d / 2.0
+    
+    # Calculate the change in water height
+    height_change = d_before - d_after
+    
+    # Calculate the volume of water (in milliliters)
+    # Using the formula for the volume of a cylinder V = π * r^2 * h, where h is the height change
+    volume = math.pi * (r ** 2) * height_change
+    
+    # Convert the volume from cubic centimeters to milliliters (1 cm^3 = 1 ml)
+    amount = volume
+    
+    return amount
+
+# Example usage
+
+
 
 try:
     while True:
-        print("--------------------------------------")
-        print("now, wait for the button to be pressed")
+        print("-----------------------------------------------------")
+        # before
+        print("-----before drink water, press the button to start-----")
         GPIO.wait_for_edge(button_pin, GPIO.FALLING)
-        print("Button is pressed, now sleep 2s for water surface stable")
+        print("Button is pressed, now collect the data before drink water")
         time.sleep(2)
-        d1 = caculate_distance()
-        print("Waiting for the button to be released")
+        d_before = caculate_distance()
+        print(f"Before drinking, the distance is {d_before} cm ")
+        
         GPIO.wait_for_edge(button_pin, GPIO.RISING)
         print("Button is released")
+
+        #after
+        print("-----after drink water, press the button to start-----")
+        GPIO.wait_for_edge(button_pin, GPIO.FALLING)
+        print("Button is pressed, now collect the data after drink water")
+        time.sleep(2)
+        d_after = caculate_distance()
+        print(f"After drinking, the distance is {d_after} cm ")
+        
+        GPIO.wait_for_edge(button_pin, GPIO.RISING)
+        print("Button is released")
+        # get the amount of water and time
+        amount = get_amount(d_before, d_after)
+        time = time.localtime(time.time())
+        # print the result
+        print(f"Time: {time.tm_hour}:{time.tm_min}  Amount: {amount} ml")
 
 except KeyboardInterrupt:
     pass
