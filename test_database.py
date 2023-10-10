9114e4214df6f830efe3ef2c44ba8c9399c2d293
Baoyue Zhang <52965522+berry1104@users.noreply.github.com>
2023-10-10 16:53:59 +0800

    update simple test database

--- test_database.py
+++ test_database.py
@@ -1,31 +1,40 @@
-DIAMETER = 5
 import sqlite3
 import time
 
-# Database connection and table creation
-conn = sqlite3.connect('distance_data.db')  # Connect to or create the database file
-cursor = conn.cursor()
-# Create a table to store distance data if it doesn't exist
-cursor.execute('''CREATE TABLE IF NOT EXISTS distance_data (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    distance REAL
-                )''')
-conn.commit()
+DIAMETER = 5
+
+def create_table():
+    try:
+        with sqlite3.connect('water_data_test.db') as conn:
+            cursor = conn.cursor()
+
+            cursor.execute('''CREATE TABLE IF NOT EXISTS water_data
+               (id INTEGER PRIMARY KEY AUTOINCREMENT, now TEXT, amount REAL)''')
+    except sqlite3.Error as e:
+        print("SQLITE ERROR", e)
+
+def insert_data(amount, now):
+    try:
+        with sqlite3.connect('water_data_test.db') as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("INSERT INTO water_data (now, amount) VALUES (?, ?)", (now, amount))
+            conn.commit()
+    except sqlite3.Error as e:
+        print("SQLITE ERROR", e)
 
 def get_amount(DIAMETER):
+    # You can modify this function to calculate the amount based on DIAMETER
     return 100
 
-for i in range(10):
+# Create the table if it doesn't exist
+create_table()
 
+for i in range(10):
     amount = get_amount(DIAMETER)
     now = time.ctime()
     print(f" -------- - {i}th test ----------")
     print("now is", now)
     print("amount is", amount)
 
-
-
-except KeyboardInterrupt:
-    #GPIO.cleanup()
-    conn.close()
+    insert_data(amount, now)
