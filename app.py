4ef9bbe7245e291ec9010fcc75f758312b309c64
lumenesce <richellechen2000@gmail.com>
2023-10-18 13:26:48 +0800

    translated changes to original file structure

--- app.py
+++ app.py
@@ -1,16 +1,75 @@
 from flask import Flask, render_template, jsonify
 import sqlite3
 import subprocess
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-app.config['STATIC_FOLDER'] = 'static'
-
-# 配置数据库连接信息
+# app.config['STATIC_FOLDER'] = 'static'
+app.config['DEBUG'] = True
 db_name = 'water_data_test.db'
+
+
+### GET DATA FROM THE LAST 24 HOURS
+def get_data_since(time_delta):
+    # Calculate the time range
+    now = datetime.now()
+    start_date = now - time_delta
+
+    # Connect to your database
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    # Query to fetch data from the start_date to now
+    cursor.execute("SELECT * FROM water_data WHERE now >= ?", (start_date,))
+    data_24 = cursor.fetchall()
+
+    conn.close()
+    return data_24
+
+
+def get_last_24_hours_data():
+    try:
+        # Calculate the timestamp for 24 hours ago
+        time_24_hours_ago = datetime.now() - timedelta(hours=24)
+        
+        with sqlite3.connect('water_data_test.db') as conn:
+            cursor = conn.cursor()
+            # Fetch records that are newer than 24 hours ago
+            cursor.execute("SELECT * FROM water_data WHERE now > ?", (time_24_hours_ago.strftime('%Y-%m-%d %H:%M:%S'),))
+            data_24 = cursor.fetchall()
+            return data_24
+    except sqlite3.Error as e:
+        print("SQLITE ERROR", e)
+        return None
+
+@app.route('/data/last_24_hours', methods=['GET'])
+def last_24_hours():
+    data = get_last_24_hours_data()
+    # if data is not None:
+        # Convert the data to a format suitable for your chart, if necessary
+    return jsonify(data)
+    # else:
+        # return jsonify({'error': 'Unable to fetch data'}), 500
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data')
+def data():
+    # Connect to your database
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM water_data")
+    data = cursor.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    # Convert the data to a format that can be sent as a response
+    return jsonify(data)
 
 @app.route('/get_data')
 def get_data():
@@ -48,5 +107,50 @@ def stop_data_collection():
     subprocess.Popen(['pkill', '-f', 'water_BCM.py'])
     return 'Data collection stopped'
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+### IGNORE
+# @app.route('/data/<string:period>')
+# def get_data(period):
+#     # Calculate the time range based on the period
+#     if period == "24h":
+#         time_threshold = datetime.now() - timedelta(hours=24)
+#     elif period == "week":
+#         time_threshold = datetime.now() - timedelta(days=7)
+#     else:  # Assuming "month" as default for simplicity
+#         time_threshold = datetime.now() - timedelta(days=30)
+
+#     # Connect to your database
+#     conn = sqlite3.connect(db_name)
+#     cursor = conn.cursor()
+
+#     # Query to fetch data based on the time period
+#     cursor.execute("SELECT * FROM water_data WHERE timestamp >= ?", (time_threshold,))
+#     data = cursor.fetchall()
+
+#     conn.close()
+
+#     return jsonify(data)
+
+# @app.route('/data/24h')
+# def data_24h():
+#     data = get_data_since(timedelta(days=1))  # last 24 hours
+#     return jsonify(data)
+
+# @app.route('/data/week')
+# def data_week():
+#     data = get_data_since(timedelta(weeks=1))  # last week
+#     return jsonify(data)
+
+# @app.route('/data/month')
+# def data_month():
+#     data = get_data_since(timedelta(days=30))  # last month
+#     return jsonify(data)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,259 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // This function sets the water level background
+    function setWaterLevelBackground(percentage) {
+
+        // Set the background of the body to a linear gradient
+        document.body.style.backgroundImage = `linear-gradient(
+            to top,
+            #98CCFFAA 0%,
+            #98CCFFAA ${percentage}%,
+            #98CCFF30 ${percentage}%,
+            #98CCFF30 100%
+        )`;
+
+        // This makes the gradient background cover the entire body, with no repetition
+        document.body.style.backgroundSize = 'cover';
+    }
+
+    // Function to update the header message with water consumption info
+    function updateWaterConsumptionHeader(totalDrank) {
+        const remaining = Math.max(0, 2000 - totalDrank); // Ensure it doesn't go negative
+
+        // Construct the message
+        const message = `You have had ${totalDrank}mL today. Drink ${remaining}mL more to reach your 2000mL daily goal.`;
+
+        // Select the header element and update its content
+        document.getElementById('waterConsumptionHeader1').textContent = `${totalDrank}mL / ${remaining}mL`;
+        document.getElementById('waterConsumptionHeader2').textContent = message;
+    }
+
+    // Fetch data from the Flask app
+    fetch('/data')
+        .then(response => response.json())
+        .then(data => {
+            // Process the data
+            let totalDrank = data.reduce((acc, entry) => acc + entry[2], 0);
+            if (totalDrank > 2000) {
+                totalDrank = 2000; // Cap at 2000mL
+            }
+
+            // Calculate the percentage of the screen to fill
+            let percentage = (totalDrank / 2000) * 100;
+
+            // Set the water level background
+            setWaterLevelBackground(percentage);
+
+            // Update the header message based on the total water drank
+            updateWaterConsumptionHeader(totalDrank);
+            
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    
+
+        
+    const progressCtx = document.getElementById('progressChart').getContext('2d');
+    const waterCup = document.getElementById('water-level');
+
+    let progressChart = new Chart(progressCtx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [0, 2000], // Initial data, will be updated
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(200, 200, 200, 0.5)'
+                ],
+            }],
+            labels: [
+                'Drank',
+                'Remaining'
+            ]
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: true,
+        }
+    });
+
+    // Fetch data from the Flask app
+    fetch('/data')
+        .then(response => response.json())
+        .then(data => {
+            // Process the data and update the chart
+            let totalDrank = data.reduce((acc, entry) => acc + entry[2], 0);
+            if (totalDrank > 2000) {
+                totalDrank = 2000;
+            }
+            let remaining = 2000 - totalDrank; // 2000ml is the total capacity
+            progressChart.data.datasets[0].data = [totalDrank, remaining];
+            progressChart.update();
+
+            // Update the cup visualization based on the data
+            let cupHeight = 200; // The total height of the SVG container
+            let waterLevel = (remaining / 2000) * cupHeight; // Calculate the water level based on the remaining amount
+            // Ensure water level is within the bounds
+            // waterLevel = Math.max(0, Math.min(waterLevel, cupHeight));
+
+            waterCup.setAttribute('height', waterLevel.toString());
+            waterCup.setAttribute('y', (cupHeight - waterLevel).toString());
+        });
+
+    // // Function to update the line chart based on the selected time period
+    // function updateLineChart(period) {
+    //     fetch(`/data/${period}`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             // let labels = data.map(entry => new Date(entry[0]).toLocaleTimeString()); // Adjust if your timestamp format differs
+    //             let labels = data.map(entry => entry[0]);
+    //             let dataPoints = data.map(entry => entry[2]); // Adjust based on where your data point is within each entry
+
+    //             // If the chart already exists, destroy it before creating a new one
+    //             if (window.lineChartInstance) {
+    //                 window.lineChartInstance.destroy();
+    //             }
+
+    //             window.lineChartInstance = new Chart(document.getElementById('lineChart'), {
+    //                 type: 'line',
+    //                 data: {
+    //                     labels: labels,
+    //                     datasets: [{
+    //                         label: 'Water Consumption',
+    //                         data: dataPoints,
+    //                         fill: false,
+    //                         borderColor: 'rgb(75, 192, 192)',
+    //                         tension: 0.1
+    //                     }]
+    //                 },
+    //                 options: {
+    //                     responsive: true,
+    //                     maintainAspectRatio: false,
+    //                     scales: {
+    //                         x: {
+    //                             type: 'time',
+    //                             time: {
+    //                                 unit: period === '24h' ? 'hour' : 'day'
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             });
+    //         });
+    // }
+
+    // // Initial chart load
+    // updateLineChart('24h'); // Default period
+
+    // // Event listener for the dropdown change
+    // document.getElementById('timePeriod').addEventListener('change', function() {
+    //     let selectedPeriod = this.value;
+    //     updateLineChart(selectedPeriod);
+    // });
+
+
+
+    // fetch('/data/last_24_hours')
+    // .then(response => response.json())
+    // .then(data => {
+    //     // Assuming 'data' is an array of records and each record is an array like [id, timestamp, amount]
+    //     const labels = data.map(record => record[1]); // extract timestamps
+    //     const dataPoints = data.map(record => record[2]); // extract amounts
+
+    //     // If the chart already exists, destroy it before creating a new one
+    //     if (window.lineChartInstance) {
+    //         window.lineChartInstance.destroy();
+    //     }
+
+    //     window.lineChartInstance = new Chart(document.getElementById('lineChart'), {
+    //         type: 'line',
+    //         data: {
+    //             labels: labels,
+    //             datasets: [{
+    //                 label: 'Water Consumption',
+    //                 data: dataPoints,
+    //                 fill: false,
+    //                 borderColor: 'rgb(75, 192, 192)',
+    //                 tension: 0.1
+    //             }]
+    //         },
+    //         options: {
+    //             responsive: true,
+    //             maintainAspectRatio: false,
+    //             scales: {
+    //                 x: {
+    //                     type: 'time',
+    //                     time: {
+    //                         unit: period === '24h' ? 'hour' : 'day'
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     });
+    // })
+    // .catch(error => {
+    //     console.error('Error fetching data:', error);
+    // });
+
+
+
+
+
+
+
+
+    // Function to create/update the line chart
+    function createLineChart(data) {
+        // Prepare data points and labels from the data
+        // let labels = data.map(entry => entry[0]); // Assuming entry[0] is your time data
+        // let dataPoints = data.map(entry => entry[2]); // Assuming entry[2] is your measurement data
+
+        const labels = data.map(record => record[1]); // extract timestamps
+        const dataPoints = data.map(record => record[2]); // extract amounts
+
+        // Create the line chart
+        let ctx = document.getElementById('lineChart').getContext('2d');
+        let lineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Water Intake',
+                    data: dataPoints,
+                    fill: true,
+                    borderColor: 'blue',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                // ... [any specific options you want for the line chart] ...
+            }
+        });
+    }
+
+    // Fetch the data and create the chart
+    fetch('/data/last_24_hours') // Make sure this URL is correct and the server is returning the expected data
+        .then(response => response.json())
+        .then(data => {
+            createLineChart(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 // JavaScript 代码
 const waterLevel = document.getElementById('water-level');
 const amountDisplay = document.getElementById('amount-display');
